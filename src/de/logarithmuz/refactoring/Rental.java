f98f53bbc398c5c496ba7317689bc6c71cf21b7b
polarode <40320189+polarode@users.noreply.github.com>
2019-04-25 11:14:41 +0200

    formated everything

--- src/de/logarithmuz/refactoring/Rental.java
+++ src/de/logarithmuz/refactoring/Rental.java
@@ -1,16 +1,19 @@
 package de.logarithmuz.refactoring;
 
 class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
-    }
-    public int getDaysRented() {
-        return daysRented;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
 }

1d75de76beac2d1673fa0b3166fc8fb81181b83f
polarode <40320189+polarode@users.noreply.github.com>
2019-04-25 12:27:25 +0200

    Extract frequentRenterPoints()

--- src/de/logarithmuz/refactoring/Rental.java
+++ src/de/logarithmuz/refactoring/Rental.java
@@ -36,4 +36,10 @@ class Rental {
 		}
 		return result;
 	}
+
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			return 2;
+		return 1;
+	}
 }

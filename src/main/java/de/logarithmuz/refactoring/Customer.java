cb2d72d7cc0e3ff6441ef6a4ba747139842418b2
polarode <40320189+polarode@users.noreply.github.com>
2019-04-27 17:03:04 +0200

    Added htmlStatement()

--- src/main/java/de/logarithmuz/refactoring/Customer.java
+++ src/main/java/de/logarithmuz/refactoring/Customer.java
@@ -41,10 +41,22 @@ class Customer {
 		return result;
 	}
 
+	public String htmlStatement() {
+		Enumeration rentals = this.rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H!><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+		return result;
+	}
+
 	private int getTotalFrequentRenterPoints() {
 		int result = 0;
 		Enumeration rentals = this.rentals.elements();
-		while (rentals.hasMoreElements()){
+		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
 			result += each.getFrequentRenterPoints();
 		}
@@ -54,7 +66,7 @@ class Customer {
 	private double getTotalCharge() {
 		double result = 0;
 		Enumeration rentals = this.rentals.elements();
-		while (rentals.hasMoreElements()){
+		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
 			result += each.getCharge();
 		}

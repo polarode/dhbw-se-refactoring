2990c0626d856b65d4965498b64e2f0f8067714f
polarode <40320189+polarode@users.noreply.github.com>
2019-04-28 11:49:34 +0200

    Replaced Conditional with Polymorphism

--- src/main/java/de/logarithmuz/refactoring/Movie.java
+++ src/main/java/de/logarithmuz/refactoring/Movie.java
@@ -5,43 +5,39 @@ public class Movie {
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
 	private String title;
-	private int priceCode;
+	Price price;
 
 	public Movie(String newtitle, int newpriceCode) {
 		title = newtitle;
-		priceCode = newpriceCode;
+		setPriceCode(newpriceCode);
 	}
 
 	public int getPriceCode() {
-		return priceCode;
+		return price.getPriceCode();
 	}
 
-	public void setPriceCode(int arg) {
-		priceCode = arg;
+	public void setPriceCode(int priceCode) {
+		switch (priceCode) {
+			case REGULAR:
+				price = new RegularPrice();
+				break;
+			case CHILDRENS:
+				price = new ChildrensPrice();
+				break;
+			case NEW_RELEASE:
+				price = new NewReleasePrice();
+				break;
+			default:
+				throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-			case REGULAR:
-				result += 2;
-				if (daysRented > 2)
-					result += (daysRented - 2) * 1.5;
-				break;
-			case NEW_RELEASE:
-				result += daysRented * 3;
-				break;
-			case CHILDRENS:
-				result += 1.5;
-				if (daysRented > 3)
-					result += (daysRented - 3) * 1.5;
-				break;
-		}
-		return result;
+	public double getCharge(int daysRented){
+		return price.getCharge(daysRented);
 	}
 
 	public int getFrequentRenterPoints(int daysRented) {
---
+++ src/main/java/de/logarithmuz/refactoring/Price.java
@@ -0,0 +1,46 @@
+package de.logarithmuz.refactoring;
+
+abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+}
+
+class ChildrensPrice extends Price {
+	public int getPriceCode() {
+		return Movie.CHILDRENS;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3) {
+			result += (daysRented - 3) * 1.5;
+		}
+		return result;
+	}
+}
+
+class NewReleasePrice extends Price {
+	public int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+
+}
+
+class RegularPrice extends Price {
+	public int getPriceCode() {
+		return Movie.REGULAR;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2) {
+			result += (daysRented - 2) * 1.5;
+		}
+		return result;
+	}
+}
